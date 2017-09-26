54741f098fc75a10f7f50cfd296266590feffb7b
Christopher Durham <cad97@cad97.com>
2017-09-25 21:51:28 -0400

    Return the rest of the non _hidden res keys

--- src/get/index.js
+++ src/get/index.js
@@ -5,12 +5,30 @@ export default [
       // I can't just pass through req here because of circular structure
       // So I've selected out some of the main properties that aren't
       res.send({
+        readable: req.readable,
+        domain: req.domain,
+        // socket: req.socket,
+        // connection: req.connection,
         httpVersionMajor: req.httpVersionMajor,
         httpVersionMinor: req.httpVersionMinor,
         httpVersion: req.httpVersion,
+        complete: req.complete,
         headers: req.headers,
+        rawHeaders: req.rawHeaders,
+        trailers: req.trailers,
+        rawTrailers: req.rawTrailers,
+        upgrade: req.upgrade,
+        url: req.url,
+        method: req.method,
+        statusCode: req.statusCode,
+        statusMessage: req.statusMessage,
+        // client: req.client,
+        // log: req.log,
+        serverName: req.serverName,
+        // timers: req.timers,
         params: req.params,
         context: req.context,
+        route: req.route,
         query: req.query
       })
       return next()
