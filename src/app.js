6c20676662ab6274f06a2d1c449eeee91874721d
cad97 <cad97@cad97.com>
2017-10-11 16:59:49 -0400

    WIP

--- src/app.js
+++ src/app.js
@@ -3,9 +3,48 @@ import '../scripts/env'
 
 import Restify from 'restify'
 import Sessions from 'client-sessions'
+import Passport from 'passport'
+import { OIDCStrategy } from 'passport-azure-ad'
 import * as Database from './database'
 import * as Paths from './paths'
-import { SESSION_SECRET } from './secrets'
+import { SESSION_SECRET, AZURE_APP_ID } from './secrets'
+
+Passport.use(new OIDCStrategy({
+  identityMetadata: 'https://login.microsoftonline.com/furman.onmicrosoft.com/.well-known/openid-configuration',
+  responseType: 'code',
+  responseMode: 'form_post',
+  redirectURL: 'http://localhost:5000/auth/openid/return/',
+  passReqToCallback: false,
+  allowHttpForRedirectUrl: true,
+  scope: ['email'],
+  clientID: AZURE_APP_ID
+},
+  function (iss, sub, profile, accessToken, refreshToken, done) {
+    if (!profile.email) {
+      return done(new Error('No email found'), null)
+    }
+    console.log(iss)
+    console.log(sub)
+    console.log(profile)
+    console.log(accessToken)
+    console.log(refreshToken)
+    done(null, null)
+    // asynchronous verification, for effect...
+    // process.nextTick(function () {
+    //   findByEmail(profile.email, function (err, user) {
+    //     if (err) {
+    //       return done(err)
+    //     }
+    //     if (!user) {
+    //     // "Auto-registration"
+    //       users.push(profile)
+    //       return done(null, profile)
+    //     }
+    //     return done(null, user)
+    //   })
+    // })
+  }
+))
 
 const server = Restify.createServer({
   name: ''
@@ -33,11 +72,38 @@ server.use(function cors (req, res, next) {
   next()
 })
 
-Paths.gets.forEach(({path, handler}) => server.get(path, handler))
-Paths.posts.forEach(({path, handler}) => server.post(path, handler))
-Paths.puts.forEach(({path, handler}) => server.put(path, handler))
-Paths.patches.forEach(({path, handler}) => server.patch(path, handler))
-Paths.deletes.forEach(({path, handler}) => server.del(path, handler))
+Paths.gets.forEach(({ path, handler }) => server.get(path, handler))
+Paths.posts.forEach(({ path, handler }) => server.post(path, handler))
+Paths.puts.forEach(({ path, handler }) => server.put(path, handler))
+Paths.patches.forEach(({ path, handler }) => server.patch(path, handler))
+Paths.deletes.forEach(({ path, handler }) => server.del(path, handler))
+
+// GET /auth/openid
+//   Use Passport.authenticate() as route middleware to authenticate the
+//   request. The first step in OpenID authentication involves redirecting
+//   the user to their OpenID provider. After authenticating, the OpenID
+//   provider redirects the user back to this application at
+//   /auth/openid/return.
+server.get('/auth/openid',
+  Passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
+  function (req, res) {
+    console.log('Authentication was called in the Sample')
+    res.redirect('/')
+  }
+)
+
+// GET /auth/openid/return
+//   Use Passport.authenticate() as route middleware to authenticate the
+//   request. If authentication fails, the user is redirected back to the
+//   sign-in page. Otherwise, the primary route function is called,
+//   which, in this example, redirects the user to the home page.
+server.get('/auth/openid/return',
+  Passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
+  function (req, res) {
+    console.log('We received a return from AzureAD.')
+    res.redirect('/')
+  }
+)
 
 export async function start () {
   try {
