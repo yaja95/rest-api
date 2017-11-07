948bd05fca37953f7acf69512bb5133430dcc869
Christopher Durham <cad97@cad97.com>
2017-11-06 19:03:05 -0500

    Add logout path

--- src/paths/login.js
+++ src/paths/login.js
@@ -1,21 +1,31 @@
 import { authenticate } from '../passport'
 
-export const get = {
-  path: '/login',
-  handler: [
-    (req, res, next) => {
-      req.session.return = req.query.return || '/'
+export const gets = [
+  {
+    path: '/login',
+    handler: [
+      (req, res, next) => {
+        req.session.return = req.query.return || '/'
+        next()
+      },
+      authenticate(),
+      (req, res, next) => {
+        let ret = req.session.return || '/'
+        req.session.return = undefined
+        delete req.session.return
+        res.redirect(ret, next)
+      }
+    ]
+  },
+  {
+    path: '/logout',
+    handler (req, res, next) {
+      req.logout()
+      res.send({})
       next()
-    },
-    authenticate(),
-    (req, res, next) => {
-      let ret = req.session.return || '/'
-      req.session.return = undefined
-      delete req.session.return
-      res.redirect(ret, next)
     }
-  ]
-}
+  }
+]
 
 export const post = {
   path: '/login/azure/return',
