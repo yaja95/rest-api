3a86db17445a1be2fb4261ca9bc4006329e8098e
Unknown <hugo.andrez.lagos@gmail.com>
2017-11-27 16:39:08 -0500

    Added editing/updating capabilities to Blog, Courses, and Events.

--- src/paths/blog.js
+++ src/paths/blog.js
@@ -63,6 +63,22 @@ async function putBlog (req, res, next) {
   next()
 }
 
+async function updateBlog (req, res, next) {
+  const id = parseInt(req.params.id)
+  if (id) {
+    const blog = await Database.BlogEntry.findById(id)
+    if (blog) {
+      await blog.update(req.body)
+      res.send(blog)
+    } else {
+      res.send(new NotFoundError('Blog not found'))
+    }
+  } else {
+    res.send(new InvalidArgumentError('Blog id must be an int'))
+  }
+  next()
+}
+
 export const gets = [
   {
     path: '/blog/drafts',
@@ -90,5 +106,9 @@ export const puts = [
   {
     path: 'blog/putBlog',
     handler: putBlog
+  },
+  {
+    path: '/blog/:id',
+    handler: updateBlog
   }
 ]
